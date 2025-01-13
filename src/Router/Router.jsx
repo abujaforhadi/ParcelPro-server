64dbedf455c2c93afbdba3b6a6d1fea2f5743084
abujaforhadi <abujaforhadi@gmail.com>
2025-01-14 01:43:15 +0600

    setup

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -1,10 +1,19 @@
 import { createBrowserRouter } from "react-router-dom";
+import MainLayouts from "../Layouts/MainLayouts";
+import Home from "../Pages/Home";
 
 const router = createBrowserRouter([
     {
-      path: "/",
-      element: <div>Hello world!</div>,
+        path: "/",
+        element: <MainLayouts />,
+        children: [
+            {
+                path: "/",
+                element: <Home />
+            },
+
+        ]
     },
-  ]);
+]);
 
 export default router;
